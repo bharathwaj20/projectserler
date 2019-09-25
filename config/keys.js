60a322b63efb16c515b9d5afcfeb6b5ccab304eb
Santosh Balaaji <sv.santosh2695@gmail.com>
2019-09-25 16:31:47 +1200

    added key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "mongoURI": "mongodb+srv://Santosh2695:sanbal2695@cluster0-xwxaw.mongodb.net/SerlerDB?retryWrites=true&w=majority"
-
+    "mongoURI": "mongodb+srv://Santosh2695:sanbal2695@cluster0-xwxaw.mongodb.net/SerlerDB?retryWrites=true&w=majority",
+    "secretOrKey" : "secret"
 }
