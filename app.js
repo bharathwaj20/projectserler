1b4de12db07b0717765ef9a26c220ae1cce08ced
Santosh Balaaji <sv.santosh2695@gmail.com>
2019-09-25 13:08:39 +1200

    merge-fix1

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const mongoose =require('mongoose');
-const bodyParser = require('body-parser');
+var mongoose =require('mongoose');
 const path =require('path');
-//const config = require('config');
 const app = express();
-const db = "mongodb+srv://Santosh2695:sanbal2695@cluster0-xwxaw.mongodb.net/test?retryWrites=true&w=majority";
+const db = require('../config/keys').mongoURI;
 mongoose.connect(db,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log('Mongo Connected')).catch(err => console.log(err));
-const user_model = require('./model/User_details');
+const user_model = require('../model/User_details');
 app.use(express.json());
 
-
-//BodyParser middleware
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-);
-app.use(bodyParser.json());
 // Read All Entries
 app.get('/', (req,res) =>{
 user_model.find().then(items => console.log( res.json(items)));
@@ -50,41 +40,10 @@ app.put('/:id',(req,res) =>{
 });
 
 
-const port =5000;
+const port = process.env.port || 5000;
 app.listen(port, ()=> console.log('Server Started on port ${port}'));
 
 
 
 
 
-
-/*mongoose.connect('mongodb+srv://Santosh2695:sanbal2695@cluster0-xwxaw.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, "connection error"));
-db.once('open',function(){
-    console.log('success');
-});
-
-var user_schema = new mongoose.Schema({
-    First_Name : 'String',
-    Last_name : 'String',
-    EmailID : { type :'String', require: true},
-    Password : 'String',
-    Access_Type : 'String'
-
-});
-user_schema.methods.GetFullName = function(){
-    return this.First_Name + this.Last_name
-
-}
-
-var users = mongoose.model('user_model',user_schema);
-var santosh = new users({First_Name: 'Santosh', Last_name : 'Balaaji', EmailID : 'sv.santosh', Password : '12345', Access_Type: 'Student'});
-var fullName = santosh.GetFullName();
-console.log(fullName);
-
-users.find({'FirstName' : 'Santosh'},function(err)
-{
-if(err) return handleError(err);
-else console.log();
-});*/
